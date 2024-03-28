f5d2fd8d645c03de173b9f87b6ae423f59a4f91d
liang.justinloi@gmail.com <liang.justinloi@gmail.com>
2024-03-28 14:36:41 -0700

    server.js cleanup

--- server/server.js
+++ server/server.js
@@ -9,29 +9,45 @@ app.get('/', (req, res) => {
   res.send('all good')
 })
 
+// app.get('/qa/questions/', controllers.getQuestions)
+
+// app.get('/qa/questions/:question_id/answers', controllers.getAnswers)
+
+// app.put('/qa/questions/:question_id/helpful', (req, res) => {
+//   console.log(req.params)
+//   res.status(204)
+// })
+
+// app.put('/qa/questions/:question_id/report', (req, res) => {
+//   console.log(req.params)
+//   res.status(204)
+// })
+
+// app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+//   console.log(req.params)
+//   res.status(204)
+// })
+
+// app.put('/qa/answers/:answer_id/report', (req, res) => {
+//   console.log(req.params)
+//   res.status(204)
+// })
+
 app.get('/qa/questions/', controllers.getQuestions)
 
 app.get('/qa/questions/:question_id/answers', controllers.getAnswers)
 
-app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  console.log(req.params)
-  res.status(204)
-})
+app.post('/qa/questions', controllers.addQuestion)
 
-app.put('/qa/questions/:question_id/report', (req, res) => {
-  console.log(req.params)
-  res.status(204)
-})
+app.post('/qa/questions/:question_id/answers', controllers.addAnswer)
 
-app.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  console.log(req.params)
-  res.status(204)
-})
+app.put('/qa/questions/:question_id/helpful', controllers.makeQuestionHelpful)
 
-app.put('/qa/answers/:answer_id/report', (req, res) => {
-  console.log(req.params)
-  res.status(204)
-})
+app.put('/qa/questions/:question_id/report', controllers.reportQuestion)
+
+app.put('/qa/answers/:answer_id/helpful', controllers.makeAnswerHelpful)
+
+app.put('/qa/answers/:answer_id/report', controllers.reportAnswer)
 
 
 app.listen(process.env.HOSTPORT, ()=>{console.log('listening on port:', process.env.HOSTPORT)})
